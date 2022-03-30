5d9d4d70d86a118e3653de99f5cc11beb3d39883
Spring Zhang <spring.okay@gmail.com>
2022-03-30 12:19:38 +0800

    Add token api, update contract address

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0x1D42C8Ef93baa95101577A06B61943a74A61C349";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://nft-collection-sable-beta.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
